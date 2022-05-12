93973f2e2d505e9bcb5b5efbb6fff165db05d59e
MyoMyintAung <myomyintaung064@gmail.com>
2022-05-12 15:00:12 +0630

    dispatch notification

--- src/App.js
+++ src/App.js
@@ -2,29 +2,86 @@ import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisiable);
+
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+
   // cart အပြောင်းအလဲ ဖြစ်တာနဲ့
   // ပြောင်းတာ ဘာလို့သိလဲဆို useSelector က redux store ကို auto subscription လုပ်ပေးလို့
   const cart = useSelector((state) => state.cart);
 
   // useEffect ထဲက ကောင်တွေ အလုပ်လုပ်မယ်
   useEffect(() => {
-    if (cart.items.length > 0) {
-      fetch("https://react-5826f-default-rtdb.firebaseio.com/cart.json", {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
+    const sendHTTP = async () => {
+      // pending state
+      dispatch(
+        uiActions.showNotification({
+          status: "pending...",
+          title: "Sending...",
+          message: "Sending cart items....",
+        })
+      );
+      const response = await fetch(
+        "https://react-5826f-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      // error state
+      if (!response.ok) {
+        throw new Error("Error");
+      }
+
+      // success state
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart items successfullu!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
     }
-  }, [cart]);
+
+    sendHTTP().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart items failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -4,10 +4,18 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     cartIsVisiable: false,
+    notification: null,
   },
   reducers: {
     toggle(state) {
       state.cartIsVisiable = !state.cartIsVisiable;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
